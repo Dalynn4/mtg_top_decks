3ce0af4178ec8c730eee2937e9d418bcf6ea5635
Randall Robinson <keith_adams007@yahoo.com>
2017-08-09 22:49:37 +0000

    working intial CLI

--- lib/mtg_top_decks/cli.rb
+++ lib/mtg_top_decks/cli.rb
@@ -1,7 +1,21 @@
 class CommandLine
 
   def call
-    puts "welcome"
+    puts "welcome to MtG: Top Decks. Please type a number of a metagame to view recent winning decklists: Type 'exit' to quit."
+    puts "1. Standard"
+    puts "2. Modern"
+    puts "3. Legacy"
+    input = gets.chomp
+    if input.to_i == 1
+      puts "standard decklists"
+    elsif input.to_i == 2
+      puts "modern decklists"
+    elsif input.to_i == 3
+      puts "legacy decklists"
+    elsif input == "exit"
+      puts "Thank you for using MtG: Top Decks!"
+    else
+      puts "Please enter a valid input."
+    end
   end
-
 end
